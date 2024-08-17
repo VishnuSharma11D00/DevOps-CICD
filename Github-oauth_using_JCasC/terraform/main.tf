0772671476490df214e0a88bd96f2f9f149c61e3
VishnuSharma11D00 <155800287+VishnuSharma11D00@users.noreply.github.com>
2024-08-17 18:57:00 +0530

    Create main.tf

---
+++ Github-oauth_using_JCasC/terraform/main.tf
@@ -0,0 +1,93 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins_sg"
+  description = "Security group for Jenkins instance"
+  # vpc_id      = "your_vpc_id"  # Replace with your VPC ID if not using the default VPC
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "jenkins" {
+  ami           = "ami-04a81a99f5ec58529"
+  instance_type = "t2.2xlarge"
+  key_name      = "aws_keypair_user_thammu"  # Using the existing key pair
+  user_data     = file("user_data.sh")
+  
+  root_block_device {
+    volume_size           = 30
+    volume_type           = "gp2"
+    delete_on_termination = true
+  }
+
+  security_groups = [aws_security_group.jenkins_sg.name]
+
+  iam_instance_profile = aws_iam_instance_profile.jenkins_instance_profile.name
+
+  tags = {
+    Name = "jenkins"
+  }
+}
+
+resource "aws_iam_instance_profile" "jenkins_instance_profile" {
+  name = "jenkins_instance_profile"
+
+  role = aws_iam_role.jenkins_role.name
+}
+
+resource "aws_iam_role" "jenkins_role" {
+  name = "jenkins_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "admin_policy" {
+  role       = aws_iam_role.jenkins_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
