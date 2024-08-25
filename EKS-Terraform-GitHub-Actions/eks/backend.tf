41ea0133242b2ef0601984d386b87d8f6f9ac48b
VishnuSharma11D00 <155800287+VishnuSharma11D00@users.noreply.github.com>
2024-08-25 16:08:09 +0530

    Update backend.tf

--- EKS-Terraform-GitHub-Actions/eks/backend.tf
+++ EKS-Terraform-GitHub-Actions/eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "vishnusharma11d00-terraform-statefile"
     region         = "us-east-1"
-    key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    key            = "terraform/jenkins/terraform.tfstate"
+    dynamodb_table = "terraform-lock"
     encrypt        = true
   }
 }
